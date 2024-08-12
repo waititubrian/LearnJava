cac8cdb44eb314bae52620e83dbca11954afc20a
waititubrian <waititubrian9@gmail.com>
2024-08-12 12:06:33 +0300

    Arithmetic Expressions added

---
+++ src/ArithmeticExpressions.java
@@ -0,0 +1,23 @@
+public class ArithmeticExpressions {
+    public static void main(String[] arg) {
+        int result = 10 / 3;
+        System.out.println(result);
+
+         double result2 = (double)10 / (double)3;
+        System.out.println(result2);
+
+        int x = 1;
+        int y = ++x;
+        System.out.println(x);
+        System.out.println(y);
+
+        int z = 1;
+        z += 2;
+        System.out.println(z);
+
+        int u = 10 + 3 * 2;
+        System.out.println(u);
+
+
+    }
+}
