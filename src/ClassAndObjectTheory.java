1766a528e8b8bf33970395dcabf21ede4125faf5
waititubrian <waititubrian9@gmail.com>
2025-07-08 00:17:37 +0300

    methods out of main class created

--- src/ClassAndObjectTheory.java
+++ src/ClassAndObjectTheory.java
@@ -19,5 +19,32 @@ public class ClassAndObjectTheory {
         String str = obj.getMeAPen(1);
         System.out.println(str);
 
+        String greeting = "HelloHelloHello";
+        int greetLength = greeting.length();
+        int number = 4;
+//        int total = greetLength + number;
+        int total = add(greetLength, number);
+        System.out.println(total);
+        System.out.println(">>>>>>>>>>>>>>>>>>");
+
+        for(int i = 0; i < greetLength; i++){
+            System.out.println(greeting.charAt(i));
+        }
+
+        int likes =4035;
+        int dislikes =4053;
+        double total2 = ratio(likes, dislikes);
+        System.out.println(total2);
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static double ratio (int likes, int dislikes){
+        if(likes + dislikes == 0) {
+            return 0;
+        }
+        return Math.round((double) likes / (likes + dislikes) * 100);
     }
 }
